0e1a0b6f487c74f510d0b659750de76a3688746d
unknown <djab501@ELCLAS408.its.york.ac.uk>
2015-02-02 15:53:05 +0000

    foo

--- mn_param_adder.vhd
+++ mn_param_adder.vhd
@@ -10,31 +10,31 @@ entity mn_param_adder is
 	generic (adder_size : integer := 8;
 				sub_adder_size : integer :=2);
 	
-    Port ( A : in  STD_LOGIC_VECTOR (size-1 downto 0);
-           B : in  STD_LOGIC_VECTOR (size-1 downto 0);
+    Port ( A : in  STD_LOGIC_VECTOR (adder_size-1 downto 0);
+           B : in  STD_LOGIC_VECTOR (adder_size-1 downto 0);
            Cin : in  STD_LOGIC;
-           S : out  STD_LOGIC_VECTOR (size-1 downto 0);
+           S : out  STD_LOGIC_VECTOR (adder_size-1 downto 0);
            Cout : out  STD_LOGIC);
 end mn_param_adder;
 
 architecture Behavioral of mn_param_adder is
 
-	signal carry : std_logic_vector (adder_size downto 0);
+	signal carry : std_logic_vector (adder_size/sub_adder_size downto 0);
 
 begin
 	mn_adder: for i in 0 to ((adder_size/sub_adder_size)-1) generate
 		p_adder: entity work.param_adder
 		generic map (adder_size => sub_adder_size)
 		port map (
-			A => A((i*sub_adder_size) to (((i+1)*sub_adder_size)-1)),
-			B => B((i*sub_adder_size) to (((i+1)*sub_adder_size)-1)), 
+			A => A(((i+1)*sub_adder_size)-1 downto ((i*sub_adder_size))),
+			B => B(((i+1)*sub_adder_size)-1 downto ((i*sub_adder_size))), 
 			Cin => carry(i), 
 			Cout => carry(i+1), 
-			S => S((i*sub_adder_size) to (((i+1)*sub_adder_size)-1)));
+			S => S(((i+1)*sub_adder_size)-1 downto ((i*sub_adder_size))));
 	end generate;
 	
 	carry(0) <= Cin;
-	Cout <= carry(adder_size);
+	Cout <= carry(adder_size/sub_adder_size);
 
 
 end Behavioral;
